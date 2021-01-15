9e6e959e0bb70f755e411484513251b0d4230157
realrlgus <realrlgus@naver.com>
2021-01-15 12:57:08 +0900

    5장 완료

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,14 @@
-import React , { Component } from "react";
-import EventPractice from './EventPractice';
-
-const App = () => {
-  return <EventPractice/>;
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
